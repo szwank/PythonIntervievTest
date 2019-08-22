4b35ed5445eccea4f6aa4dc202f47deb5dc2afa6
Szwank <lukpanki@gmail.com>
2019-08-22 14:25:58 +0200

    Added classes for storing and manipulating movies

---
+++ backend/fetch_data.py
@@ -0,0 +1,17 @@
+
+
+class FetchMovies:
+    def __init__(self):
+        pass
+
+    def fetch_movie(self):
+        pass
+
+    def add_movie_to_database(self):
+        pass
+
+    def __initialize_movies_data_base(self):
+        pass
+
+    def find_movie_by_name(self, name):
+        pass
