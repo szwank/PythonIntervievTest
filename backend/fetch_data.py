06c7a6ed814c89cf17f3886173d37df38d989228
Szwank <lukpanki@gmail.com>
2019-08-23 11:39:50 +0200

    added fetching movie

--- backend/fetch_data.py
+++ backend/fetch_data.py
@@ -1,13 +1,33 @@
+from google.appengine.api import urlfetch
+from backend import error
+import logging
+from google.appengine.ext import webapp
 
+class ErrorWhileFetching(error.Error):
+    pass
 
-class FetchMovies:
+class FetchMovie:
+    access_key = 'apikey=ffdb4910'
+    url = 'http://www.omdbapi.com/'
+
     def __init__(self):
         pass
 
-    def fetch_movie(self):
-        pass
+    def fetch_movie(self, *args):
+        url_attributes = "&".join(args)
+        try:
+            fetched_url = self.url + '?' + url_attributes + '&' + self.access_key
+            result = urlfetch.fetch(fetched_url)
 
-    def add_movie_to_database(self):
+            if result.status_code == 200:
+                return result.content
+            else:
+                logging.error(result.status_code)
+        except:
+            raise ErrorWhileFetching("While fetching %s error was occur" % fetched_url)
+
+    @classmethod
+    def add_movie_to_database(cls):
         pass
 
     def __initialize_movies_data_base(self):
@@ -15,3 +35,11 @@ class FetchMovies:
 
     def find_movie_by_name(self, name):
         pass
+
+# class Print(webapp.RequestHandler):
+#     def get(self):
+#         self.response.header['Content-type'] = 'text/plain'
+#         self.response.write('hello')
+
+
+
