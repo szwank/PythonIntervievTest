31102c7bacd4ee8469a874ec169b1b0406580007
Szwank <lukpanki@gmail.com>
2019-08-23 12:19:02 +0200

    creation Movie now works

--- backend/movie.py
+++ backend/movie.py
@@ -1,18 +1,19 @@
 from google.appengine.ext import ndb
 from backend.keyvalue import KeyValue
-
+import logging
 
 class Movie(ndb.Model):
     title = ndb.StringProperty(indexed=True)
-    data = ndb.KeyProperty(KeyValue, repeated=True)
+    data = ndb.KeyProperty(KeyValue)
 
     @classmethod
     def create(cls, title, data):
         data = KeyValue.create(title, data)
         entity = cls(title=title,
-                     data=data)
+                     data=data.key)
         entity.put()
-        return entity
+        logging.error('done')
+        # return entity
 
     @classmethod
     def get(cls):
