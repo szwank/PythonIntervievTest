01175d94b2189d7037dd9d5628c0b8af4b70dcfa
Szwank <lukpanki@gmail.com>
2019-08-23 18:57:19 +0200

    added function to get Movie titles

--- backend/fetch_data.py
+++ backend/fetch_data.py
@@ -1,26 +1,30 @@
 from google.appengine.api import urlfetch
 from backend import error
 import logging
-import random
-import json
-import movie
+
 
 
 class ErrorWhileFetching(error.Error):
     pass
 
+
 class MoviesAreInDataBase(error.Error):
     pass
+
 
 class FetchMovie:
     access_key = 'apikey=ffdb4910'
     url = 'http://www.omdbapi.com/'
 
     @classmethod
+    def create_fetched_url(cls, url_attributes):
+        return cls.url + '?' + url_attributes + '&' + cls.access_key
+
+    @classmethod
     def fetch_movie(cls, *args):
         url_attributes = "&".join(args)
         try:
-            fetched_url = cls.url + '?' + url_attributes + '&' + cls.access_key
+            fetched_url = cls.create_fetched_url(url_attributes)
             result = urlfetch.fetch(fetched_url)
 
             if result.status_code == 200:
@@ -30,33 +34,11 @@ class FetchMovie:
         except:
             raise ErrorWhileFetching("While fetching %s error was occur" % fetched_url)
 
-    @classmethod
-    def add_many_movies_to_database(cls, how_much_to_add):
-        movies_id_to_fetch = random.sample(range(70000), how_much_to_add)
-        for i in range(how_much_to_add):
-            argument = 'i=tt' + "%07d" % movies_id_to_fetch[i]
-            result = cls.fetch_movie(argument)
-            cls.add_movie_to_database(data=result)
-
-    def __initialize_movies_data_base(self):
-        pass
-
-    @classmethod
-    def add_movie_to_database(self, data):
-        data_dictionary = json.loads(data)
-        title = data_dictionary.get('Title', 'No title')
-        movie.Movie.create(title, data)
 
 
 
-    def find_movie_by_name(self, name):
-        pass
 
 
-# class Print(webapp.RequestHandler):
-#     def get(self):
-#         self.response.header['Content-type'] = 'text/plain'
-#         self.response.write('hello')
 
 
 
--- backend/movie.py
+++ backend/movie.py
@@ -1,29 +1,84 @@
 from google.appengine.ext import ndb
-from backend.keyvalue import KeyValue, Key
+import random
+from backend import keyvalue
 from backend import fetch_data
 import logging
+import json
+
+from backend.cache import lru_cache, mem_cache
 
 Empty = []
 
-class Movie(KeyValue):
+class Movie(keyvalue.KeyValue):
+    title = ndb.StringProperty(indexed=True)
+
+    @classmethod
+    def create(cls, key, title, value, expires=None):
+        entity = super(cls, Movie).create(key, value, expires)
+        entity.update(title=title)
+
+    @classmethod
+    def add_many_movies_to_database(cls, how_much_to_add):
+        movies_id_to_fetch = random.sample(range(70000), how_much_to_add)
+        for i in range(how_much_to_add):
+            argument = 'i=tt' + "%07d" % movies_id_to_fetch[i]
+            result = fetch_data.FetchMovie.fetch_movie(argument)
+            set(result)
 
     @classmethod
     def initialize_movie_database(cls, how_much_to_add):
-        # if cls.query(Movie).fetch(1) is not None:
         if cls.query().fetch(1) == Empty:
-            # logging.error(cls.query().fetch(1))
-            fetch_data.FetchMovie.add_many_movies_to_database(how_much_to_add=how_much_to_add)
-
+            cls.add_many_movies_to_database(how_much_to_add=how_much_to_add)
         else:
             # Movies are in database no need for adding them
             logging.info("Movies are still in data base. None was added")
-            pass
+
 
     @classmethod
     def delete_all_movies(cls):
         list_of_keys = ndb.put_multi(cls.query())
         list_of_entities = ndb.get_multi(list_of_keys)
-        ndb.delete_multi(list_of_keys)
+        ndb.delete_multi(list_of_entities)
 
+    @classmethod
+    @lru_cache()
+    @mem_cache()
+    def get_titles(cls, titles_on_single_page=10):
+        list_of_titles = map(lambda x: str(x.title), cls.query().fetch(titles_on_single_page))
+        list_of_titles.sort()
+        return list_of_titles
+
+
+    @classmethod
+    def find_movie_by_name(cls, name):
+        pass
+
+
+
+
+def set(value, namespace=None, expires=None):
+    if type(value) is str:
+        value = json.loads(value)
+    elif type(value) is dict:
+        # Correct data, do nothing
+        pass
+    else:
+        raise ValueError("Incorrect data")
+
+    title = value.get("Title", "Not named")
+    key = keyvalue.Key(title, namespace=namespace)
+    entity = Movie.create(key, title, json.dumps(value), expires)
+    return key if entity is not None else None
+
+def get(title, namespace=None):
+    key = keyvalue.Key(title, namespace=namespace)
+    entity = Movie.get(key)
+    return json.loads(entity.value) if entity and not entity.expired() else None
+
+def delete(title, namespace=None):
+    key = keyvalue.Key(title, namespace=namespace)
+    entity = Movie.get(key)
+    if entity:
+        entity.delete()
 
 
