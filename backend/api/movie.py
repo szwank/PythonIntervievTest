f0d10853e82127b4342f835d12fd0f0cc11fca2b
Szwank <lukpanki@gmail.com>
2019-08-24 20:44:18 +0200

    added class Titles for Services

--- backend/api/movie.py
+++ backend/api/movie.py
@@ -1,7 +1,22 @@
+from backend import api
+from backend import movie
+from protorpc import remote, messages, message_types
 
-from google.appengine.ext import webapp
+
+class GetTitle(messages.Message):
+    get = messages.StringField(1, required=True)
+
+class Titles(messages.Message):
+    titles = messages.StringField(1, required=True)
+
+@api.endpoint(path="title", title="Movie API")
+class Titles(remote.Service):
+    @remote.method(GetTitle, Titles)
+    def get(self, reguest):
+        titles = movie.Movie.get_titles
+        Titles(titles=titles)
+        return Titles
 
 
-class Titles(webapp.RequestHandler):
-    def get(self):
-        
+
+
