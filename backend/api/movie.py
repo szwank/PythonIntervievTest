3d3060c1c513286b2481287ed0ea92e47444f03f
Szwank <lukpanki@gmail.com>
2019-08-25 19:08:53 +0200

    added remove movie by id and removed bugs from other methods

--- backend/api/movie.py
+++ backend/api/movie.py
@@ -1,19 +1,24 @@
 from backend import api
 from protorpc import remote, messages, message_types
-from backend import movie
-from backend import fetch_data
+from backend import movie, fetch_data
+from backend.oauth2 import oauth2
+
 
 class JsonField(messages.StringField):
     type = dict
 
+
 class GetTitle(messages.Message):
     get = messages.StringField(1, required=True)
+
 
 class Title(messages.Message):
     title = messages.StringField(1, required=True)
 
+
 class Titles(messages.Message):
     titles = messages.MessageField(Title, 1, repeated=True)
+
 
 class GetMoviesRequest(messages.Message):
     how_many = messages.IntegerField(1, default=10)
@@ -23,52 +28,66 @@ class GetMoviesRequest(messages.Message):
 
     order = messages.EnumField(Order, 3, default=Order.TITLE)
 
+
 class GetSingleMovieRequest(messages.Message):
     title = messages.StringField(1, required=True)
 
+
 class Movie(messages.Message):
-    movie = JsonField(1, required=True)
+    title = messages.StringField(1, required=True)
+    ID = messages.IntegerField(2, required=True)
+    description = JsonField(3, required=True)
+
 
 class Movies(messages.Message):
     movies = messages.MessageField(Movie, 1, repeated=True)
+
 
 class AddMovieRequest(messages.Message):
     title = messages.StringField(1, required=True)
 
 
-# class Title(messages.Message):
-#     title = message
-#
+class RemoveMovieByTitleRequest(messages.Message):
+    ID = messages.IntegerField(1, required=True)
+
+
 @api.endpoint(path="movie", title="Movie API")
 class MovieService(remote.Service):
 
     @remote.method(GetMoviesRequest, Movies)
     def get_movies(self, request):
-        movies = movie.get_movies(10)
+        movies = movie.Movie.get_movies(request.how_many)
 
         if request.order == GetMoviesRequest.Order.TITLE:
-            movies.sort(key=lambda item: item.get("Title"))
+            movies.sort(key=lambda item: item.description.get("Title"))
 
-        movies = map(lambda element: Movie(movie=element), movies)
+        map_to_movie = lambda item: Movie(title=item.title,
+                                          ID=item.key.id(),
+                                          description=item.description)
+
+        movies = map(map_to_movie, movies)
         return Movies(movies=movies)
 
     @remote.method(GetSingleMovieRequest, Movie)
     def get_movie_by_title(self, request):
-        result = movie.get(request.title)
+        result = movie.Movie.get_by_title(request.title)
 
         if result:
-            return Movie(movie=result)
+            return Movie(title=result.title, ID=result.key.id(), description=result.description)
         else:
             return Movie(movie={'No Results': 'There is no matching title'})
 
     @remote.method(AddMovieRequest, message_types.VoidMessage)
     def add_movie(self, request):
-        fetch_result = fetch_data.FetchMovie.fetch_movie_by_title(request.title)
-        movie.set(fetch_result)
+        movie_description = fetch_data.FetchMovie.fetch_movie_by_title(request.title)
+        movie.Movie.create_from_decription(movie_description)
         return message_types.VoidMessage()
 
-
-
+    @remote.method(RemoveMovieByTitleRequest, message_types.VoidMessage)
+    @oauth2.required()
+    def remove_movie_by_ID(self, request):
+        movie.Movie.delete_by_ID(request.ID)
+        return message_types.VoidMessage()
 
 
 
