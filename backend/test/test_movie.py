ff579585f3a53d00d4547473251eb8f32f16c0a1
Szwank <lukpanki@gmail.com>
2019-08-26 16:29:29 +0200

    added more tests

--- backend/test/test_movie.py
+++ backend/test/test_movie.py
@@ -1,15 +1,122 @@
-from backend import test, movie
+from backend import test, movie, fetch_data
+from google.appengine.ext import ndb
+import time
+import json
+
 
 class TestMovie(test.TestCase):
 
-    def test_create(self):
-        description_without_title = "{'Website': 'N/A', 'Production': 'Universal', 'Actors': 'Faith Domergue, Richard Long, Marshall Thompson, Kathleen Hughes', \
-              'Metascore': 'N/A', 'Runtime': '82 min', 'imdbVotes': '837', 'Year': '1955', 'Director': 'Francis D. Lyon', 'Response': 'True', \
-              'Rated': 'Approved', 'Awards': 'N/A', \
-              'DVD': '13 Apr 1994', 'Type': 'movie', 'Released': '05 Aug 1955', \
-              'Genre': 'Fantasy, Horror', 'BoxOffice': 'N/A', 'Plot': \
-              'American G.I.s who trespass on a Hindu ceremony are hunted down by a beautiful woman who has the power to transform herself into a cobra.', \
-              'Country': 'USA', 'imdbRating': '5.8', 'imdbID': 'tt0047966', 'Language': 'English'}"
+    description_without_title = {'Website': 'N/A', 'Production': 'Universal',
+                                 'Actors': 'Faith Domergue, Richard Long, Marshall Thompson, Kathleen Hughes',
+                                 'Metascore': 'N/A', 'Runtime': '82 min', 'imdbVotes': '837', 'Year': '1955',
+                                 'Director': 'Francis D. Lyon', 'Response': 'True',
+                                 'Rated': 'Approved', 'Awards': 'N/A',
+                                 'DVD': '13 Apr 1994', 'Type': 'movie', 'Released': '05 Aug 1955',
+                                 'Genre': 'Fantasy, Horror', 'BoxOffice': 'N/A', 'Plot':
+                                     'American G.I.s who trespass on a Hindu ceremony are hunted down by a beautiful woman who has the power to transform herself into a cobra.',
+                                 'Country': 'USA', 'imdbRating': '5.8', 'imdbID': 'tt0047966', 'Language': 'English'}
+    title = 'Cult of the Cobra'
 
-        obj = movie.Movie.create(title='Cult of the Cobra', description=description_without_title, put_into_database=False)
-        self.a
+    wrong_format_of_description = "{'Type': 'movie', 'Released': '05 Aug 1955'}}"
+
+    description_with_title = {'Website': 'N/A', 'Production': 'Universal',
+                                 'Actors': 'Faith Domergue, Richard Long, Marshall Thompson, Kathleen Hughes',
+                                 'Metascore': 'N/A', 'Runtime': '82 min', 'imdbVotes': '837', 'Year': '1955',
+                                 'Director': 'Francis D. Lyon', 'Response': 'True',
+                                 'Rated': 'Approved', 'Awards': 'N/A',
+                                 'DVD': '13 Apr 1994', 'Type': 'movie', 'Released': '05 Aug 1955',
+                                 'Genre': 'Fantasy, Horror', 'BoxOffice': 'N/A', 'Plot':
+                                     'American G.I.s who trespass on a Hindu ceremony are hunted down by a beautiful woman who has the power to transform herself into a cobra.',
+                                 'Country': 'USA', 'imdbRating': '5.8', 'imdbID': 'tt0047966',
+                                 'Language': 'English', 'Title': 'Cult of the Cobra'}
+
+
+    def test_create(self):
+        obj = movie.Movie.create(title=self.title, description=self.description_without_title, put_into_database=False)
+        self.assertIsNot(obj, movie.Movie.get_by_title(title=self.title))
+        obj.put()
+        self.assertEqual(obj, movie.Movie.get_by_ID(ID=obj.key.id()))
+        self.assertTrue(obj.description == self.description_without_title)
+        self.assertTrue(obj.title == self.title)
+        self.assertRaises(movie.TitleTaken, lambda: movie.Movie.create(title=self.title, description=self.description_without_title))
+        self.assertRaises(ValueError, lambda: movie.Movie.create(title=self.title, description=123))
+        self.assertRaises(ValueError, lambda: movie.Movie.create(title=self.title, description=self.wrong_format_of_description))
+
+    def test_create_from_description(self):
+
+        obj = movie.Movie.create_from_decription(description=dict(self.description_with_title), put_into_database=False)
+        self.assertIsNot(obj, movie.Movie.get_by_title(title=self.title))
+        obj.put()
+        self.assertEqual(obj, movie.Movie.get_by_ID(ID=obj.key.id()))
+        self.assertTrue(obj.description == self.description_without_title)
+        self.assertTrue(obj.title == self.title)
+        self.assertRaises(movie.TitleTaken,
+                          lambda: movie.Movie.create_from_decription(description=dict(self.description_with_title)))
+        self.assertRaises(ValueError, lambda: movie.Movie.create_from_decription(description=123))
+        self.assertRaises(ValueError, lambda: movie.Movie.create_from_decription(description=self.wrong_format_of_description))
+        self.assertRaises(ValueError, lambda: movie.Movie.create_from_decription(description=self.description_without_title))
+
+    def test_create_from_list_of_descriptions(self):
+        self.fetch_movie_mock.set_description(description=self.description_with_title)
+        descriptions = fetch_data.FetchMovie.fetch_random_movies(10)
+
+        objects = movie.Movie.create_from_list_of_descriptions(descriptions)
+        self.assertEqual(len(objects), 10)
+        self.assertListEqual(objects, map(lambda obj: movie.Movie.get_by_ID(ID=obj.id()).key, objects))
+
+    def test_add_movies_to_database(self):
+        self.fetch_movie_mock.set_description(description=self.description_with_title)
+        objects = movie.Movie.add_movies_to_database(10)
+
+        self.assertEqual(len(objects), 10)
+        self.assertListEqual(objects, map(lambda obj: movie.Movie.get_by_ID(ID=obj.id()).key, objects))
+
+    def test_delete_all_movies(self):
+        self.fetch_movie_mock.set_description(description=self.description_with_title)
+        movie.Movie.add_movies_to_database(10)
+
+        movie.Movie.delete_all_movies()
+        self.assertFalse(movie.Movie.query().fetch())
+
+    def test_initialize_movie_database(self):
+        self.fetch_movie_mock.set_description(description=self.description_with_title)
+        movie.Movie.delete_all_movies()
+
+        movie.Movie.initialize_movie_database(how_much_to_add=10)
+        self.assertEqual(len(movie.Movie.query().fetch()), 10)
+
+        movie.Movie.initialize_movie_database(how_much_to_add=10)
+        self.assertEqual(len(movie.Movie.query().fetch()), 10)        # check if number of movies in database has not changed
+
+    def test_delete_by_ID(self):
+        obj = movie.Movie.create_from_decription(self.description_with_title)
+
+        self.assertIsNone(movie.Movie.delete_by_ID(obj.key.id()))
+        self.assertIsNone(movie.Movie.query(Key=obj.key.id()))
+
+    def test_get_by_title(self):
+        obj = movie.Movie.create_from_decription(self.description_with_title)
+
+        taken_obj = movie.Movie.get_by_title(self.title)
+
+        self.assertTrue(obj.title == taken_obj.title)
+        self.assertEqual(obj.key.id(), taken_obj.key.id())
+        self.assertTrue(obj.description == taken_obj.description)
+
+        movie.Movie.delete_by_ID(obj.key.id())
+
+        self.assertIsNone(movie.Movie.get_by_title(self.title))
+
+    def test_get_by_ID(self):
+        obj = movie.Movie.create_from_decription(self.description_with_title)
+
+        taken_obj = movie.Movie.get_by_ID(obj.key.id())
+
+        self.assertTrue(obj.title == taken_obj.title)
+        self.assertEqual(obj.key.id(), taken_obj.key.id())
+        self.assertTrue(obj.description == taken_obj.description)
+
+        movie.Movie.delete_by_ID(obj.key.id())
+
+        self.assertIsNone(movie.Movie.get_by_ID(obj.key.id()))
+
