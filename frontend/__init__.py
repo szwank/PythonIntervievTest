a5e6c382d99f901a160ce9c14ed33b9801d42458
Szwank <lukpanki@gmail.com>
2019-08-26 21:33:42 +0200

    removed ewbapp

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import webapp
-from routes import Index
+from routes import Index, Delete_all_movies
 
-app = webapp.WSGIApplication([('/index', Index)
+app = webapp.WSGIApplication([('/index', Index),
+                              ('/delete', Delete_all_movies)
                               ], debug=True)
 
