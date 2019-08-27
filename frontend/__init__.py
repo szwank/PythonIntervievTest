9d9a14cbf95be9782d24486dd0a1042a37500fc4
Szwank <lukpanki@gmail.com>
2019-08-27 07:43:34 +0200

    removed unnecesery files

--- frontend/__init__.py
+++
@@ -1,7 +0,0 @@
-from google.appengine.ext import webapp
-from routes import Index, Delete_all_movies
-
-app = webapp.WSGIApplication([('/index', Index),
-                              ('/delete', Delete_all_movies)
-                              ], debug=True)
-
